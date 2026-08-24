bf683f1826d1f6f07570acbc6b9df27efed0fbeb
divkumarsingh <divyanshusingh1101@gmail.com>
2026-08-24 13:31:09 +0000

    added database

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,16 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ["esbuild"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "replit.com",
+        pathname: "/cdn-cgi/image/**"
+      }
+    ]
+  }
 };
 
 export default nextConfig;
